5420f17d8ddddd9f269135299ea71856c742dd2e
Maliksidk19 <malik.sid1980@gmail.com>
2023-04-05 20:28:14 +0500

    Connecting to database using mongodb driver

--- server.js
+++ server.js
@@ -3,14 +3,22 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3500;
-const mongoose = require("mongoose");
+const { MongoClient } = require("mongodb");
 
-try {
-  mongoose.connect(process.env.MONGO_URI);
-  console.log("Connected successfully to Database...");
-} catch (err) {
-  console.error(err);
-}
+const client = new MongoClient(process.env.MONGO_URI);
+
+const connectToDatabase = async () => {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB Successfully !!");
+  } catch (err) {
+    console.error("Error Conneting to Database", err);
+  } finally {
+    await client.close();
+  }
+};
+
+connectToDatabase();
 
 app.listen(PORT, () => {
   console.log(`Server listening on "http://localhost:${PORT}"`);
